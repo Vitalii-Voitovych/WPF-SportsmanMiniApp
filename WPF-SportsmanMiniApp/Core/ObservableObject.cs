a4da266712eaf9e38f84f4fc1f872e07a3404653
Vitalii Voitovych <vitalikvv2119@gmail.com>
2022-01-06 21:14:04 +0200

    Добавьте файлы проекта.

---
+++ WPF-SportsmanMiniApp/Core/ObservableObject.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace WPF_SportsmanMiniApp.Core
+{
+    public class ObservableObject : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+
+        }
+    }
+}
